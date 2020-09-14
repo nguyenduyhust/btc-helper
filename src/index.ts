7ef67ac759745c5a880f3adbec074ba9960e234c
Duy Nguyen <duynb@icts.vn>
2020-09-14 16:15:44 +0700

    Update index

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,2 @@
-export { BitgoHelper } from "./bitgo";
-export { AddressInfo as BitgoAddressInfo } from "./bitgo/interfaces";
-
-export { BlockStreamHelper } from "./blockstream";
-export { BlockStreamRoutesEnum as BSRoutesEnum } from "./blockstream/enums";
-export {
-  Block as BSBlock,
-  Transaction as BSTransaction,
-} from "./blockstream/interfaces";
-
-export { NetworkNamesEnum } from "./enums";
+export * from "./bitgo";
+export * from "./blockstream";
